37a6f3fb239ca6ff53e4d6111668fca4fe085483
William Zale <williamzale@gmail.com>
2023-05-18 12:13:09 -0400

    test: Add tests for above_market_average, rooms_from_category, area, and details

--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -7,21 +7,58 @@ RSpec.describe House do
     
     @room_1 = Room.new(:bedroom, 10, '13')
     @room_2 = Room.new(:bedroom, 11, '15') 
+    @room_3 = Room.new(:living_room, 25, '15')
+    @room_4 = Room.new(:basement, 30, '41')
   end
 
-  it "exists with readable attributes" do
-    expect(@house).to be_an_instance_of(House)
-    expect(@house.price).to eq(400000)
-    expect(@house.address).to eq("123 sugar lane")
-    expect(@house.rooms).to eq([])
+  describe "Iteration 2" do
+    it "exists with readable attributes" do
+      expect(@house).to be_an_instance_of(House)
+      expect(@house.price).to eq(400000)
+      expect(@house.address).to eq("123 sugar lane")
+      expect(@house.rooms).to eq([])
+    end
+    
+    it "has an add_rooms method" do
+      expect(@house.rooms).to eq([])
+      
+      @house.add_room(@room_1)
+      @house.add_room(@room_2)
+      
+      expect(@house.rooms).to eq([@room_1, @room_2])
+    end
   end
   
-  it "has an add_rooms method" do
-    expect(@house.rooms).to eq([])
+  describe "Iteration 3" do
+    it "has an above_market_average? method" do
+      expect(@house.above_market_average?).to eq(false)
+    end
     
-    @house.add_room(@room_1)
-    @house.add_room(@room_2)
-    
-    expect(@house.rooms).to eq([@room_1, @room_2])
+    it "has a rooms_from_category method" do
+      @house.add_room(@room_1)
+      @house.add_room(@room_2)
+      @house.add_room(@room_3)
+      @house.add_room(@room_4)
+      
+      expect(@house.rooms_from_category(:bedroom)).to eq([@room_1, @room_2])
+    end
+
+    it "has an area method" do
+      @house.add_room(@room_1)
+      @house.add_room(@room_2)
+      @house.add_room(@room_3)
+      @house.add_room(@room_4)
+      
+      expect(@house.area).to eq(1900)
+    end
+
+    it "has a details method" do
+      @house.add_room(@room_1)
+      @house.add_room(@room_2)
+      @house.add_room(@room_3)
+      @house.add_room(@room_4)
+      
+      expect(@house.details).to eq({"price" => 400000, "address" => "123 sugar lane"})
+    end
   end
 end
