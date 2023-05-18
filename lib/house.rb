7f13f7dadf21f45500b719314e98b59153eb4c16
William Zale <williamzale@gmail.com>
2023-05-18 12:11:11 -0400

    feat: Add above_market_average?, rooms_from_category, and area methods, as well as details instance variable

--- lib/house.rb
+++ lib/house.rb
@@ -1,15 +1,29 @@
 class House
   attr_reader :price, 
               :address,
-              :rooms
+              :rooms,
+              :details
 
   def initialize(price, address)
     @price = price.delete("$").to_i
     @address = address
     @rooms = []
+    @details = {"price" => @price, "address" => @address}
   end
 
   def add_room(room)
     @rooms << room
+  end
+
+  def above_market_average?
+    @price > 500000
+  end
+
+  def rooms_from_category(category)
+    @rooms.find_all { |room| room.category == category }
+  end
+
+  def area
+    @rooms.sum { |room| room.area }
   end
 end
